706aa1463486042baed39e2aa77e09f29fe314c4
Tiana Akins <tiana.aakins95@gmail.com>
2024-03-06 14:02:08 -0600

    Initial Commit

---
+++ Scrumdinger(iOSAppDevTutorial)/Scrumdinger_iOSAppDevTutorial_App.swift
@@ -0,0 +1,17 @@
+//
+//  Scrumdinger_iOSAppDevTutorial_App.swift
+//  Scrumdinger(iOSAppDevTutorial)
+//
+//  Created by Tiana Akins on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Scrumdinger_iOSAppDevTutorial_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Scrumdinger(iOSAppDevTutorial)UITests/Scrumdinger_iOSAppDevTutorial_UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Scrumdinger_iOSAppDevTutorial_UITestsLaunchTests.swift
+//  Scrumdinger(iOSAppDevTutorial)UITests
+//
+//  Created by Tiana Akins on 3/6/24.
+//
+
+import XCTest
+
+final class Scrumdinger_iOSAppDevTutorial_UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
